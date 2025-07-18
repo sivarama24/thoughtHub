42e6bd2915cd268ab89a6a01c088684ad11bd619
Sivaramalingam <sr5992745@gmail.com>
2025-07-18 23:53:52 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+let posts = [];
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.get("/compose", (req, res) => {
+    res.render("compose.ejs");
+})
+
+app.get("/community", (req, res) => {
+    res.render("community.ejs", { posts: posts });
+})
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+})
+
+app.post("/compose", (req, res) => {
+    const post = {
+        title: req.body["postTitle"],
+        name: req.body["authorName"],
+        content: req.body["postContent"],
+    }
+
+    posts.push(post);
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${ port }`);
+})
---
+++ public/script.js
@@ -0,0 +1,3 @@
+function toggleMenu() {
+    document.getElementById("navItems").classList.toggle("show");
+}
