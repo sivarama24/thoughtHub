1dc30a342cbe4d4a2af7fef343e5aed0a68daa7d
Sivaramalingam <sr5992745@gmail.com>
2025-07-19 00:47:18 +0530

    Added dotenv package

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 let posts = [];
 
